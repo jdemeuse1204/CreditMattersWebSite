58525d3f2752b9e75d983a2304b7b72272f50e1c
James Demeuse <james.demeuse@gmail.com>
2017-01-30 20:48:15 -0600

    localhost change

--- src/environment.js
+++ src/environment.js
@@ -1,5 +1,5 @@
-//export const repositoryUrl = "http://localhost:18000/";
-export const repositoryUrl = "http://api.skygroundlabs.net/";
+export const repositoryUrl = "http://localhost:18000/";
+//export const repositoryUrl = "http://api.skygroundlabs.net/";
 export const rememberDeviceDays = 365; 
 export const authorizationTokenKey = "cm";
 export const deviceTokenKey = "cm_devices";
