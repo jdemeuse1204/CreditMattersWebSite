c73af9e488c11e80037f6f54ced002649e762239
James DeMeuse <james.demeuse@chrobinson.com>
2017-06-20 11:25:41 -0500

    loading screen fix

--- src/customElements/loading-screen.js
+++ src/customElements/loading-screen.js
@@ -1,17 +1,21 @@
+/* beautify preserve:start */
 import { bindable, useView, customElement } from 'aurelia-framework';
 import { PLATFORM } from 'aurelia-pal';
-
+import './loading-screen.less';
+import * as nprogress from 'nprogress';
+import 'nprogress/nprogress.css';
+/* beautify preserve:end */
 
 @customElement('loading-screen')
 @useView(PLATFORM.moduleName('./loading-screen.html'))
 export class LoadingScreen {
-  @bindable loading = true;
+  @bindable loading = false;
 
   loadingChanged(newValue) {
     if (newValue) {
-      
+      nprogress.start();
     } else {
-      
+      nprogress.done();
     }
   }
 }
